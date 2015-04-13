a87428f388b9618f4ee1efbd87148ddfd96ee0a3
Victor Wibisono <devacto@gmail.com>
2015-04-14 09:51:14 +1000

    Fixed codenarc violations on StencilServiceResource.groovy

--- src/main/groovy/au/com/dius/stencilservice/resources/StencilServiceResource.groovy
+++ src/main/groovy/au/com/dius/stencilservice/resources/StencilServiceResource.groovy
@@ -1,34 +1,34 @@
-package au.com.dius.stencilservice.resources;
+package au.com.dius.stencilservice.resources
 
-import au.com.dius.stencilservice.core.Saying;
-import com.codahale.metrics.annotation.Timed;
-import com.google.common.base.Optional;
+import au.com.dius.stencilservice.core.Saying
+import com.codahale.metrics.annotation.Timed
+import com.google.common.base.Optional
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.MediaType;
-import java.util.concurrent.atomic.AtomicLong;
+import javax.ws.rs.GET
+import javax.ws.rs.Path
+import javax.ws.rs.Produces
+import javax.ws.rs.QueryParam
+import javax.ws.rs.core.MediaType
+import java.util.concurrent.atomic.AtomicLong
 
-@Path("hello")
+@Path('hello')
 @Produces(MediaType.APPLICATION_JSON)
-public class StencilServiceResource {
-    private final String template;
-    private final String defaultName;
+class StencilServiceResource {
+    private final String template
+    private final String defaultName
 
-    private final AtomicLong counter;
+    private final AtomicLong counter
 
-    public StencilServiceResource(String template, String defaultName) {
-        this.template = template;
-        this.defaultName = defaultName;
-        this.counter = new AtomicLong();
+    StencilServiceResource(String template, String defaultName) {
+        this.template = template
+        this.defaultName = defaultName
+        this.counter = new AtomicLong()
     }
 
     @GET
     @Timed
-    public Saying sayHello(@QueryParam("name")Optional<String> name) {
-        final String value = String.format(template, name.or(defaultName));
-        return new Saying(counter.incrementAndGet(), value);
+    Saying sayHello(@QueryParam('name')Optional<String> name) {
+        final String value = String.format(template, name | defaultName)
+        new Saying(counter.incrementAndGet(), value)
     }
 }
