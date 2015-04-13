07a00470bb2be1cc1a6f21e84c1212f935354ed3
Victor Wibisono <devacto@gmail.com>
2015-04-14 09:46:36 +1000

    Fixed codenarc violations on TemplateHealthCheck.groovy

--- src/main/groovy/au/com/dius/stencilservice/health/TemplateHealthCheck.groovy
+++ src/main/groovy/au/com/dius/stencilservice/health/TemplateHealthCheck.groovy
@@ -1,23 +1,25 @@
-package au.com.dius.stencilservice.health;
+package au.com.dius.stencilservice.health
 
 import com.codahale.metrics.health.HealthCheck
 import com.codahale.metrics.health.HealthCheck.Result
-import static com.codahale.metrics.health.HealthCheck.Result.*
+import static com.codahale.metrics.health.HealthCheck.Result.healthy
+import static com.codahale.metrics.health.HealthCheck.Result.unhealthy
 
-public class TemplateHealthCheck extends HealthCheck {
-    private final String template;
+class TemplateHealthCheck extends HealthCheck {
+    public static final String TEST = 'TEST'
+    private final String template
 
-    public TemplateHealthCheck(String template) {
-        this.template = template;
+    TemplateHealthCheck(String template) {
+        this.template = template
     }
 
     @Override
     protected Result check() throws Exception {
-        final String saying = String.format(template, "TEST");
-        if (!saying.contains("TEST")) {
-            return unhealthy("Template doesn't include a name!");
+        final String saying = String.format(template, TEST)
+        if (!saying.contains(TEST)) {
+            unhealthy('Template doesn\'t include a name!')
 
         }
-        return healthy();
+        healthy()
     }
 }
